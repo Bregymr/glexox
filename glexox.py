e5ba1d3251673eaf04f06300d9d191945481d0ef
Greg Courville <Greg_Courville@GregLabs.com>
2012-12-18 04:40:54 -0800

    Added GPLv3 license blurb

--- glexox.py
+++ glexox.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#  This file is part of GLExOx.
+#
+#  GLExOx is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  GLExOx is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with GLExOx.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# (C) 2011 Greg Courville <greg_courville@greglabs.com>
+
+
 import sys
 import serial
 import array
